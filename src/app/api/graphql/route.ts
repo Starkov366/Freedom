8d8935e95a76573cdeb755698effea7bf8ad7136
Starkov52 <valuznnicartem@gmail.com>
2025-09-24 21:58:44 +0300

    fix

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,18 +1,19 @@
+// src/app/api/graphql/route.ts
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import resolvers from "@/graphQl/resolvers";
 import typeDefs from "@/graphQl/schema";
+import { NextRequest } from "next/server";
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
 const handler = startServerAndCreateNextHandler(server, {
      context: async (req) => ({ req })
 });
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
      return handler(req);
 }
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
      return handler(req);
 }
